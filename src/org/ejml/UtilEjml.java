1d70d1c51e6b5c7ede36cf4c62202d07b725837a
peter.abeles@gmail.com <peter.abeles@gmail.com@76591b1e-92a2-11de-8d68-838d6ba51ce1>
2012-08-22 20:29:32 +0000

    Final commit for 0.20 release

--- src/org/ejml/UtilEjml.java
+++ src/org/ejml/UtilEjml.java
@@ -35,7 +35,7 @@ public class UtilEjml {
     /**
      * Version string used to indicate which version of EJML is being used.
      */
-    public static String VERSION = "0.19";
+    public static String VERSION = "0.20";
 
     /**
      * Default tolerance.
