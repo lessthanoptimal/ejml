296801ac9707d2157ee28b36759c1bd0c93a8f8c
peter.abeles <peter.abeles@76591b1e-92a2-11de-8d68-838d6ba51ce1>
2013-09-23 00:45:37 +0000

    - Fixing copyright issues

--- generate/src/org/ejml/CodeGeneratorBase.java
+++ generate/src/org/ejml/CodeGeneratorBase.java
@@ -1,20 +1,6 @@
 /*
- * This file is part of Efficient Java Matrix Library (EJML).
+ * Copyright (c) 2009-2013, Peter Abeles. All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
  * This file is part of Efficient Java Matrix Library (EJML).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -45,7 +31,23 @@ import java.io.PrintStream;
 public abstract class CodeGeneratorBase {
 
     public static final String copyright =
-            "";
+            "/*\n" +
+                    " * Copyright (c) 2009-2013, Peter Abeles. All Rights Reserved.\n" +
+                    " *\n" +
+                    " * This file is part of Efficient Java Matrix Library (EJML).\n" +
+                    " *\n" +
+                    " * Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
+                    " * you may not use this file except in compliance with the License.\n" +
+                    " * You may obtain a copy of the License at\n" +
+                    " *\n" +
+                    " *   http://www.apache.org/licenses/LICENSE-2.0\n" +
+                    " *\n" +
+                    " * Unless required by applicable law or agreed to in writing, software\n" +
+                    " * distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
+                    " * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
+                    " * See the License for the specific language governing permissions and\n" +
+                    " * limitations under the License.\n" +
+                    " */";
 
     protected PrintStream out;
     protected String className;
