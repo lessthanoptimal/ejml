1680c6f54718636fa41237a0c786b13f53e71529
peter.abeles <peter.abeles@76591b1e-92a2-11de-8d68-838d6ba51ce1>
2013-10-04 22:54:17 +0000

    - minor change from ReshapeMatrix to Matrix64F

--- src/org/ejml/alg/dense/misc/ImplCommonOps_Matrix64F.java
+++ src/org/ejml/alg/dense/misc/ImplCommonOps_Matrix64F.java
@@ -18,7 +18,7 @@
 
 package org.ejml.alg.dense.misc;
 
-import org.ejml.data.ReshapeMatrix64F;
+import org.ejml.data.Matrix64F;
 
 /**
  * Implementations of common ops routines for {@link org.ejml.data.DenseMatrix64F}.  In general
@@ -27,9 +27,9 @@ import org.ejml.data.ReshapeMatrix64F;
  * @author Peter Abeles
  */
 public class ImplCommonOps_Matrix64F {
-    public static void extract(ReshapeMatrix64F src,
+    public static void extract(Matrix64F src,
                                int srcY0, int srcX0,
-                               ReshapeMatrix64F dst,
+                               Matrix64F dst,
                                int dstY0, int dstX0,
                                int numRows, int numCols )
     {
