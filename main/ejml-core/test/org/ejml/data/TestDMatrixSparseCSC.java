0eccdc6d6e515a8220c58da75169c7f3acc2c35b
Peter Abeles <peter.abeles@gmail.com>
2020-07-07 22:55:32 -0700

    - Switched version to SNAPSHOT - TestDMatrixSparseCSC   * Reduced amount of memory used by unit tests   * Thanks FlorentinD!

--- main/ejml-core/test/org/ejml/data/TestDMatrixSparseCSC.java
+++ main/ejml-core/test/org/ejml/data/TestDMatrixSparseCSC.java
@@ -47,23 +47,23 @@ public class TestDMatrixSparseCSC extends GenericTestsDMatrixSparse {
 
     @Test
     public void constructor_veryLarge() {
-        DMatrixSparseCSC a = new DMatrixSparseCSC(1_000_000_000,1_000_000_000,4);
+        DMatrixSparseCSC a = new DMatrixSparseCSC(1_000_000_000,100_000_000,4);
 
         assertEquals(0,a.nz_length);
         assertEquals(1_000_000_000,a.numCols);
-        assertEquals(1_000_000_000,a.numRows);
+        assertEquals(100_000_000,a.numRows);
         assertEquals(4,a.nz_values.length);
         assertEquals(4,a.nz_rows.length);
     }
 
     @Test
     public void growMaxLength_veryLarge() {
-        DMatrixSparseCSC a = new DMatrixSparseCSC(1_000_000_000,1_000_000_000,4);
+        DMatrixSparseCSC a = new DMatrixSparseCSC(1_000_000_000,100_000_000,4);
 
         a.growMaxLength(10,false);
         assertEquals(0,a.nz_length);
         assertEquals(1_000_000_000,a.numCols);
-        assertEquals(1_000_000_000,a.numRows);
+        assertEquals(100_000_000,a.numRows);
         assertEquals(10,a.nz_values.length);
         assertEquals(10,a.nz_rows.length);
     }
