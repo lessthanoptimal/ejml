cabbef11b1e9ff0c3c8e4425ce727e54ebd4976a
FlorentinD <florentin.doerre@tu-dresden.de>
2020-07-14 18:20:26 +0200

    Test and operations with semi-rings .. and fixing errors based on wrong identity element

---
+++ main/ejml-dsparse/test/org/ejml/sparse/csc/mult/TestMatrixMatrixMultWithSemiRing_DSCC.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2009-2020, Peter Abeles. All Rights Reserved.
+ *
+ * This file is part of Efficient Java Matrix Library (EJML).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ejml.sparse.csc.mult;
+
+import org.ejml.data.DMatrixSparseCSC;
+import org.ejml.ops.DSemiRing;
+import org.ejml.ops.DSemiRings;
+import org.ejml.sparse.csc.CommonOpsWithSemiRing_DSCC;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestMatrixMatrixMultWithSemiRing_DSCC {
+    DMatrixSparseCSC inputMatrix;
+
+    @BeforeEach
+    public void setUp() {
+        // based on example in http://mit.bme.hu/~szarnyas/grb/graphblas-introduction.pdf
+        inputMatrix = new DMatrixSparseCSC(7, 7, 12);
+        inputMatrix.set(0, 1, 1);
+        inputMatrix.set(0, 3, 1);
+        inputMatrix.set(1, 4, 1);
+        inputMatrix.set(1, 6, 1);
+        inputMatrix.set(2, 5, 1);
+        inputMatrix.set(3, 0, 0.2);
+        inputMatrix.set(3, 2, 0.4);
+        inputMatrix.set(4, 5, 1);
+        inputMatrix.set(5, 2, 0.5);
+        inputMatrix.set(6, 2, 1);
+        inputMatrix.set(6, 3, 1);
+        inputMatrix.set(6, 4, 1);
+
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("sparseVectorMatrixMultSources")
+    public void mult_v_A(String desc, DSemiRing semiRing, double[] expected) {
+        // graphblas == following outgoing edges of source nodes
+        DMatrixSparseCSC vector = new DMatrixSparseCSC(1, 7);
+        vector.set(0, 3, 0.5);
+        vector.set(0, 5, 0.6);
+
+        DMatrixSparseCSC found = CommonOpsWithSemiRing_DSCC.mult(vector, inputMatrix, null, semiRing);
+
+        assertEquals(expected[0], found.get(0, 0));
+        assertEquals(expected[1], found.get(0, 2));
+    }
+
+    private static Stream<Arguments> sparseVectorMatrixMultSources() {
+        return Stream.of(
+                // expected entries for (0, 0) and (0, 2)
+                Arguments.of("Plus, Times", DSemiRings.PLUS_TIMES, new double[]{0.1, 0.5}),
+                Arguments.of("OR, AND", DSemiRings.OR_AND, new double[]{1, 1}),
+                Arguments.of("MIN, PLUS", DSemiRings.MIN_PLUS, new double[]{0.7, 0.9}),
+                Arguments.of("MAX, PLUS", DSemiRings.MAX_PLUS, new double[]{0.7, 1.1}),
+                Arguments.of("MIN, TIMES", DSemiRings.MIN_TIMES, new double[]{0.1, 0.2}),
+                Arguments.of("MAX, MIN", DSemiRings.MAX_MIN, new double[]{0.2, 0.5})
+        );
+    }
+}
---
+++ main/ejml-dsparse/test/org/ejml/sparse/csc/mult/TestMatrixVectorMultWithSemiRing_DSCC.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2009-2020, Peter Abeles. All Rights Reserved.
+ *
+ * This file is part of Efficient Java Matrix Library (EJML).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ejml.sparse.csc.mult;
+
+import org.ejml.data.DMatrixSparseCSC;
+import org.ejml.ops.DoubleSemiRing;
+import org.ejml.ops.PreDefinedDoubleSemiRings;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestMatrixVectorMultWithSemiRing_DSCC {
+    DMatrixSparseCSC inputMatrix;
+
+    @BeforeEach
+    public void setUp() {
+        // based on example in http://mit.bme.hu/~szarnyas/grb/graphblas-introduction.pdf
+        inputMatrix = new DMatrixSparseCSC(7, 7, 12);
+        inputMatrix.set(0, 1, 1);
+        inputMatrix.set(0, 3, 1);
+        inputMatrix.set(1, 4, 1);
+        inputMatrix.set(1, 6, 1);
+        inputMatrix.set(2, 5, 1);
+        inputMatrix.set(3, 0, 0.2);
+        inputMatrix.set(3, 2, 0.4);
+        inputMatrix.set(4, 5, 1);
+        inputMatrix.set(5, 2, 0.5);
+        inputMatrix.set(6, 2, 1);
+        inputMatrix.set(6, 3, 1);
+        inputMatrix.set(6, 4, 1);
+
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("vectorMatrixMultSources")
+    public void mult_v_A(String desc, DoubleSemiRing semiRing, double[] expected) {
+        // graphblas == following outgoing edges of source nodes
+        double v[] = new double[7];
+        Arrays.fill(v, semiRing.add.id);
+        v[3] = 0.5;
+        v[5] = 0.6;
+
+        System.out.println("input vector = " + Arrays.toString(v));
+
+        //input.print();
+
+        double found[] = new double[7];
+
+        MatrixVectorMultWithSemiRing_DSCC.mult(v, inputMatrix, found, semiRing);
+
+        System.out.println("found = " + Arrays.toString(found));
+        System.out.println("expected = " + Arrays.toString(expected));
+        assertTrue(Arrays.equals(found, expected));
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("matrixVectorMultSources")
+    public void mult_A_v(String desc, DoubleSemiRing semiRing, double[] expected) {
+        // graphblas == following incoming edges of source nodes
+        double v[] = new double[7];
+        Arrays.fill(v, semiRing.add.id);
+        v[3] = 0.5;
+        v[4] = 0.6;
+
+        System.out.println("input vector = " + Arrays.toString(v));
+
+        //input.print();
+
+        double found[] = new double[7];
+
+        MatrixVectorMultWithSemiRing_DSCC.mult(inputMatrix, v, found, semiRing);
+
+        System.out.println("found = " + Arrays.toString(found));
+        System.out.println("expected = " + Arrays.toString(expected));
+        assertTrue(Arrays.equals(found, expected));
+    }
+
+    private static Stream<Arguments> vectorMatrixMultSources() {
+        return Stream.of(
+                Arguments.of("Plus, Times", PreDefinedDoubleSemiRings.PLUS_TIMES, new double[]{0.1, 0, 0.5, 0, 0, 0, 0}),
+                Arguments.of("OR, AND", PreDefinedDoubleSemiRings.OR_AND, new double[]{1, 0, 1, 0, 0, 0, 0}),
+                Arguments.of("MIN, PLUS", PreDefinedDoubleSemiRings.MIN_PLUS,
+                        new double[]{0.7, Double.MAX_VALUE, 0.9, Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE}),
+// This would only work on sparse vectors (here resulting in 1.0 instead of Double.MIN_Value)
+//                Arguments.of("MAX, PLUS", PreDefinedDoubleSemiRings.MAX_PLUS,
+//                        new double[]{0.7, Double.MIN_VALUE, 1.1, Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE}),
+                Arguments.of("MIN, TIMES", PreDefinedDoubleSemiRings.MIN_TIMES,
+                        new double[]{0.1, Double.MAX_VALUE, 0.2, Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE}),
+                Arguments.of("MAX, MIN", PreDefinedDoubleSemiRings.MAX_MIN,
+                        new double[]{0.2, Double.MIN_VALUE, 0.5, Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE})
+        );
+    }
+
+    private static Stream<Arguments> matrixVectorMultSources() {
+        return Stream.of(
+                Arguments.of("Plus, Times", PreDefinedDoubleSemiRings.PLUS_TIMES, new double[]{0.5, 0.6, 0, 0, 0, 0, 1.1}),
+                Arguments.of("OR, AND", PreDefinedDoubleSemiRings.OR_AND, new double[]{1, 1, 0, 0, 0, 0, 1}),
+                Arguments.of("MIN, PLUS", PreDefinedDoubleSemiRings.MIN_PLUS,
+                        new double[]{1.5, 1.6, Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE, 1.5})
+        );
+    }
+}
