b9f0c6d5f1888cfcf971cd2607256b8c828c3926
Peter Abeles <peter.abeles@gmail.com>
2020-09-27 07:34:30 -0700

    AutocodeConcurrentApp  - Synchronized with other version

--- main/autocode/src/org/ejml/AutocodeConcurrentApp.java
+++ main/autocode/src/org/ejml/AutocodeConcurrentApp.java
@@ -18,7 +18,6 @@
 
 package org.ejml;
 
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -29,7 +28,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static org.ejml.GenerateCode32.findPathToProjectRoot;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
  * Generates concurrent implementations of a class using hints provided in comments throughout the code.
@@ -54,7 +53,8 @@ import static org.ejml.GenerateCode32.findPathToProjectRoot;
 public class AutocodeConcurrentApp {
 
 	private static final String prefix = "//CONCURRENT_";
-	private static String tab = "    ";
+	private static final String tab = "    ";
+	private static final FindProjectRoot findRootDirectory = ()-> new File(GenerateCode32.findPathToProjectRoot());
 
 	/**
 	 * Converts the file from single thread into concurrent implementation
@@ -66,7 +66,7 @@ public class AutocodeConcurrentApp {
 		String classNameNew = className(outputFile);
 
 		// Read the file and split it up into lines
-		List<String> inputLines = readLines(original,Charset.forName("UTF-8"));
+		List<String> inputLines = readLines(original, UTF_8);
 		List<String> outputLines = new ArrayList<>();
 
 		List<Macro> macros = new ArrayList<>();
@@ -81,48 +81,49 @@ public class AutocodeConcurrentApp {
 		for (int i = 0; i < inputLines.size(); i++) {
 			String line = inputLines.get(i);
 			int where = line.indexOf(prefix);
-			if( where < 0 ) {
+			if (where < 0) {
 				if (!foundImport && !foundClassDef && line.startsWith("import")) {
 					foundImport = true;
 					outputLines.add("import javax.annotation.Generated;");
-				} else if( !foundClassDef && line.contains("class "+classNameOld)) {
+				} else if (!foundClassDef && line.contains("class " + classNameOld)) {
 					foundClassDef = true;
 					if (foundImport)
 						outputLines.add("@Generated(\"" + derivePackagePath(outputFile) + "." + classNameOld + "\")");
 					line = line.replaceFirst("class " + classNameOld, "class " + classNameNew);
-				} else if( foundImport && line.startsWith("@Generated")) {
+				} else if (foundImport && line.startsWith("@Generated")) {
 					// If the file already has a generated statement and we are going to add our own remove the old one
 					continue;
 				} else {
-					line = line.replace(classNameOld+"(",classNameNew+"(");
+					line = line.replace(classNameOld + "(", classNameNew + "(");
 				}
 				if (omit)
 					continue;
-				for( Macro m : macros )
-					line = line.replace(m.name,m.text);
+				for (Macro m : macros)
+					line = line.replace(m.name, m.text);
 				outputLines.add(line);
 				continue;
 			}
-			String type = readType(line,where+prefix.length());
-			String whitespaces = line.substring(0,where);
-			int frontLength = where+prefix.length()+type.length();
-			String message = line.length()>frontLength ? line.substring(frontLength+1) : "";
-			switch(type) {
-				case "CLASS_NAME":continue; // ignore. already processed
+			String type = readType(line, where + prefix.length());
+			String whitespaces = line.substring(0, where);
+			int frontLength = where + prefix.length() + type.length();
+			String message = line.length() > frontLength ? line.substring(frontLength + 1) : "";
+			switch (type) {
+				case "CLASS_NAME":
+					continue; // ignore. already processed
 				case "INLINE":
-					outputLines.add(whitespaces+message);
+					outputLines.add(whitespaces + message);
 					break;
 				case "ABOVE":
 					// remove the previous line
-					outputLines.remove(outputLines.size()-1);
-					outputLines.add(whitespaces+message);
+					outputLines.remove(outputLines.size() - 1);
+					outputLines.add(whitespaces + message);
 					break;
 				case "BELOW":
-					outputLines.add(whitespaces+message);
+					outputLines.add(whitespaces + message);
 					i += 1; // skip next line
 					break;
 				case "REMOVE_ABOVE":
-					outputLines.remove(outputLines.size()-1);
+					outputLines.remove(outputLines.size() - 1);
 					break;
 				case "REMOVE_BELOW":
 					i += 1; // skip next line
@@ -135,8 +136,8 @@ public class AutocodeConcurrentApp {
 					break;
 				case "MACRO": {
 					String[] words = message.split(" ");
-					if( words.length != 2)
-						throw new RuntimeException("Expected only two words for the macro. "+message);
+					if (words.length != 2)
+						throw new RuntimeException("Expected only two words for the macro. " + message);
 					Macro m = new Macro();
 					m.name = words[0];
 					m.text = words[1];
@@ -144,7 +145,7 @@ public class AutocodeConcurrentApp {
 					break;
 				}
 				default:
-					throw new RuntimeException("Unknown: "+type);
+					throw new RuntimeException("Unknown: " + type);
 			}
 		}
 
@@ -159,11 +160,10 @@ public class AutocodeConcurrentApp {
 
 	/**
 	 * If a test class doesn't exist it will create one. This is to remind the user to do it
-	 * @param file
 	 */
 	private static void createTestIfNotThere( File file ) {
-		String fileName = "Test"+file.getName();
-		String parent = file.getParent().replaceAll("src","test");
+		String fileName = "Test" + file.getName();
+		String parent = file.getParent().replaceAll("src", "test");
 
 //		List<String> packagePath = new ArrayList<>();
 //		while( true ) {
@@ -182,8 +182,8 @@ public class AutocodeConcurrentApp {
 //		for (int i = packagePath.size()-2; i >= 0; i--) {
 //			file = new File(file,packagePath.get(i));
 //		}
-		file = new File(parent,fileName);
-		if( file.exists() ) {
+		file = new File(parent, fileName);
+		if (file.exists()) {
 			return;
 		}
 		createTestFile(file);
@@ -191,11 +191,11 @@ public class AutocodeConcurrentApp {
 
 	private static void createTestFile( File path ) {
 
-		if( !path.getParentFile().exists() ) {
-			if( !path.getParentFile().mkdirs() )
-				throw new RuntimeException("Failed to create directories. "+path.getAbsolutePath());
+		if (!path.getParentFile().exists()) {
+			if (!path.getParentFile().mkdirs())
+				throw new RuntimeException("Failed to create directories. " + path.getAbsolutePath());
 		}
-		System.out.println("Creating "+path);
+		System.out.println("Creating " + path);
 
 		try {
 			String className = className(path);
@@ -209,25 +209,25 @@ public class AutocodeConcurrentApp {
 					"import static org.junit.jupiter.api.Assertions.fail;\n" +
 					"\n" +
 					"class " + className + " {\n" +
-					tab+"@Test\n" +
-					tab+"void compareToSingle() {\n" +
-					tab+tab+"fail(\"implement\");\n" +
-					tab+"}\n" +
+					tab + "@Test\n" +
+					tab + "void compareToSingle() {\n" +
+					tab + tab + "fail(\"implement\");\n" +
+					tab + "}\n" +
 					"}\n");
 			out.close();
-		} catch( FileNotFoundException e ) {
+		} catch (FileNotFoundException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
 	private static String derivePackagePath( File file ) {
 		List<String> packagePath = new ArrayList<>();
-		while( true ) {
-			if( file.getParentFile() == null )
+		while (true) {
+			if (file.getParentFile() == null)
 				throw new IllegalArgumentException("Problem! Can't find java directory");
 
 			String parent = file.getParentFile().getName();
-			if( parent.equals("test") || parent.equals("src") || parent.equals("java") ) {
+			if (parent.equals("test") || parent.equals("src") || parent.equals("java")) {
 				break;
 			} else {
 				packagePath.add(parent);
@@ -236,21 +236,22 @@ public class AutocodeConcurrentApp {
 		}
 
 		String output = "";
-		for (int i = packagePath.size()-1; i >= 0; i--) {
-			output += packagePath.get(i)+".";
+		for (int i = packagePath.size() - 1; i >= 0; i--) {
+			output += packagePath.get(i) + ".";
 		}
-		return output.substring(0,output.length()-1);
+		return output.substring(0, output.length() - 1);
 	}
 
 	/**
 	 * Searches the input file for an override. If none is found then _MT is added to the class name.
+	 *
 	 * @param original Input file
 	 * @return Output file
 	 */
 	private static File determineClassName( File original ) throws IOException {
 		String text = readFileToString(original, Charset.forName("UTF-8"));
 
-		if(!text.contains("//CONCURRENT"))
+		if (!text.contains("//CONCURRENT"))
 			throw new IOException("Not a concurrent file");
 
 		String pattern = "//CONCURRENT_CLASS_NAME ";
@@ -268,45 +269,43 @@ public class AutocodeConcurrentApp {
 			return new File(original.getParent(),name+".java");
 		}
 
-		String name = readUntilEndOfLine(text,where+pattern.length());
-		return new File(original.getParent(),name+".java");
+		String name = readUntilEndOfLine(text, where + pattern.length());
+		return new File(original.getParent(), name + ".java");
 	}
 
-	private static String readType( String line , int location ) {
+	private static String readType( String line, int location ) {
 		int index0 = location;
-		while( location < line.length() ) {
+		while (location < line.length()) {
 			char c = line.charAt(location);
-			if( Character.isWhitespace(c) ) {
-				return line.substring(index0,location);
+			if (Character.isWhitespace(c)) {
+				return line.substring(index0, location);
 			}
 			location += 1;
 		}
 		// something went wrong
-		return line.substring(index0,location);
+		return line.substring(index0, location);
 	}
 
-	private static String readUntilEndOfLine( String text , int location ) {
+	private static String readUntilEndOfLine( String text, int location ) {
 		int index0 = location;
-		while( location < text.length() ) {
+		while (location < text.length()) {
 			char c = text.charAt(location);
-			if( c == '\r' || c == '\n' ) {
-				return text.substring(index0,location);
+			if (c == '\r' || c == '\n') {
+				return text.substring(index0, location);
 			}
 			location += 1;
 		}
-		return text.substring(index0,location);
+		return text.substring(index0, location);
 	}
 
-	static String readFileToString(File file, Charset encoding)
-			throws IOException
-	{
+	static String readFileToString( File file, Charset encoding ) throws IOException {
 		byte[] encoded = Files.readAllBytes(file.toPath());
 		return new String(encoded, encoding);
 	}
 
 	private static String className( File file ) {
 		String n = file.getName();
-		return n.substring(0,n.length()-5);
+		return n.substring(0, n.length() - 5);
 	}
 
 	private static class Macro {
@@ -314,32 +313,38 @@ public class AutocodeConcurrentApp {
 		String text;
 	}
 
-	public static void convertDir( File directory , String include , String exclude ) {
-		if( !directory.isDirectory() )
-			throw new IllegalArgumentException("Must be a directory: '"+directory.getPath()+"'");
+	public static void convertDir( File directory, String include, String exclude ) {
+		if (!directory.isDirectory())
+			throw new IllegalArgumentException("Must be a directory: '" + directory.getPath() + "'");
 		File[] files = directory.listFiles();
-		if( files == null )
+		if (files == null)
 			throw new IllegalArgumentException("No files");
-		for( File f : files ) {
+		for (File f : files) {
 			String name = f.getName();
-			if( !name.matches(include) || name.matches(exclude))
+			if (!name.matches(include) || name.matches(exclude))
 				continue;
 			try {
 				convertFile(f);
-			} catch( IOException ignore ) {
+			} catch (IOException ignore) {
 //				System.out.println(name+" "+e.getMessage());
 			}
 		}
 	}
 
-	public static List<String> readLines(final File file, final Charset encoding) throws IOException {
+	public static List<String> readLines( final File file, final Charset encoding ) throws IOException {
 		List<String> lines = new ArrayList<>();
-		Stream<String> stream = Files.lines(file.toPath(),encoding);
+		Stream<String> stream = Files.lines(file.toPath(), encoding);
 		stream.forEach(lines::add);
 		return lines;
 	}
 
-	public static void main(String[] args) throws IOException {
+	public interface FindProjectRoot
+	{
+		File findPathToRoot();
+	}
+
+	public static void main( String[] args ) throws IOException {
+
 		String[] directories = new String[]{
 				"main/ejml-ddense/src/org/ejml/dense/row/mult",
 				"main/ejml-ddense/src/org/ejml/dense/row/misc",
@@ -358,17 +363,18 @@ public class AutocodeConcurrentApp {
 //				"main/boofcv-ip/src/main/java/boofcv/alg/enhance/impl/ImplEnhanceHistogram.java"
 		};
 
-		String projectRoot = findPathToProjectRoot();
-		System.out.println("Project Root: '"+projectRoot+"'");
+		File rootDir = findRootDirectory.findPathToRoot();
+		System.out.println("Autocode Concurrent: current=" + new File(".").getAbsolutePath());
+		System.out.println("                     root=" + rootDir.getAbsolutePath());
 
-		for( String f : directories ) {
-			System.out.println("directory "+f);
-			convertDir(new File(projectRoot,f),"\\S+\\.java","\\S+MT\\S+");
+		for (String f : directories) {
+			System.out.println("directory " + f);
+			convertDir(new File(rootDir, f), "\\S+\\.java", "\\S+MT\\S+");
 		}
 
-		for( String f : files ) {
-			System.out.println("File "+f);
-			convertFile(new File(f));
+		for (String f : files) {
+			System.out.println("File " + f);
+			convertFile(new File(rootDir, f));
 		}
 	}
 }
