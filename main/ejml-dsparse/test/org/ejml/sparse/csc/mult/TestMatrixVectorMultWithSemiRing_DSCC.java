f96b6dab3c0817a02503f8c16603df5830d633ea
FlorentinD <florentin.doerre@tu-dresden.de>
2020-10-08 10:41:16 +0200

    Fix bug in random matrix generator
    Resulting in the wrong row selection strategy being used.
    
    Also:
    - Add a benchmark for random sparse matrix generation
    - Suppress unused method warnings for parameterized tests methods

--- main/ejml-dsparse/test/org/ejml/sparse/csc/mult/TestMatrixVectorMultWithSemiRing_DSCC.java
+++ main/ejml-dsparse/test/org/ejml/sparse/csc/mult/TestMatrixVectorMultWithSemiRing_DSCC.java
@@ -31,6 +31,7 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SuppressWarnings("UnusedMethod")
 public class TestMatrixVectorMultWithSemiRing_DSCC {
     DMatrixSparseCSC inputMatrix;
 
