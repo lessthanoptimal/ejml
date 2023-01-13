13ff8d4dfedcff80bfe00d53e2dde238d5195af6
Peter Abeles <peter.abeles@gmail.com>
2023-01-13 15:17:47 -0800

    spotless

--- main/ejml-ddense/src/org/ejml/dense/row/decomposition/eig/watched/WatchedDoubleStepQREigenvector_DDRM.java
+++ main/ejml-ddense/src/org/ejml/dense/row/decomposition/eig/watched/WatchedDoubleStepQREigenvector_DDRM.java
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.ejml.dense.row.decomposition.eig.watched;
 
 import org.ejml.UtilEjml;
