0a0c8f92dba76b51c5d0da24ed95dbb147d6809f
David Hägele <david.haegele@visus.uni-stuttgart.de>
2023-03-15 15:21:10 +0100

    add unit test for multTransA via SimpleOperations_DDRM

--- main/ejml-simple/test/org/ejml/simple/ops/TestSimpleOperations_DDRM.java
+++ main/ejml-simple/test/org/ejml/simple/ops/TestSimpleOperations_DDRM.java
@@ -18,9 +18,13 @@
 
 package org.ejml.simple.ops;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.ejml.data.DMatrixRMaj;
+import org.ejml.dense.row.MatrixFeatures_DDRM;
 import org.ejml.dense.row.RandomMatrices_DDRM;
 import org.ejml.simple.SimpleOperations;
+import org.junit.jupiter.api.Test;
 
 class TestSimpleOperations_DDRM extends BaseSimpleOperationsChecks<DMatrixRMaj> {
     @Override public SimpleOperations<DMatrixRMaj> createOps() {
@@ -29,5 +33,22 @@ class TestSimpleOperations_DDRM extends BaseSimpleOperationsChecks<DMatrixRMaj> 
 
     @Override public DMatrixRMaj randomRect( int numRows, int numCols ) {
         return RandomMatrices_DDRM.rectangle(numRows, numCols, rand);
+    }
+    
+    @Test void multTransA() {
+    	SimpleOperations<DMatrixRMaj> ops = createOps();
+    	for(int nRows : new int[] {1_000, 10_000, 100_000}) {
+    		DMatrixRMaj randMat = randomRect(nRows, 1);
+    		DMatrixRMaj ones = new DMatrixRMaj(nRows,1);
+    		ops.fill(ones, (double) 1.0);
+
+    		DMatrixRMaj expected = new DMatrixRMaj(1, 1);
+    		expected.set(0, (double) ops.elementSum(randMat));
+
+    		DMatrixRMaj result = new DMatrixRMaj(1, 1);
+    		ops.multTransA(randMat, ones, result);
+
+    		assertTrue(MatrixFeatures_DDRM.isIdentical(expected, result, (double) 1e-20));
+    	}
     }
 }
