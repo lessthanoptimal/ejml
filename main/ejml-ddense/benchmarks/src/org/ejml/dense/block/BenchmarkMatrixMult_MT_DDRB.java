35823e34ecd19ddf2874831f91d7b5462cd7e963
Peter Abeles <peter.abeles@gmail.com>
2026-04-28 13:56:05 -0700

    BenchmarkMatrixMult_MT_DDRB - Compares different matrix

--- main/ejml-ddense/benchmarks/src/org/ejml/dense/block/BenchmarkMatrixMult_MT_DDRB.java
+++ main/ejml-ddense/benchmarks/src/org/ejml/dense/block/BenchmarkMatrixMult_MT_DDRB.java
@@ -36,63 +36,103 @@ import java.util.concurrent.TimeUnit;
 @State(Scope.Benchmark)
 @Fork(value = 1)
 public class BenchmarkMatrixMult_MT_DDRB {
-    //    @Param({"100", "500", "1000", "5000", "10000"})
     @Param({"1000"})
     public int size;
 
-    //    @Param({"5","10","20","40","80","120"})
     @Param({"60"})
     public int blockLength;
+
+    @Param({"square", "tall", "wide", "long"})
+    public String shape;
 
     public DMatrixRBlock A, B, C;
     public DSubmatrixD1 Asub, Bsub, Csub;
 
+    int M, N, P;
+
     @Setup
     public void setup() {
-        Random rand = new Random(234);
+        var rand = new Random(234);
 
-        A = MatrixOps_DDRB.createRandom(size, size, -1, 1, rand, blockLength);
-        B = MatrixOps_DDRB.createRandom(size, size, -1, 1, rand, blockLength);
-        C = MatrixOps_DDRB.createRandom(size, size, -1, 1, rand, blockLength);
+        // Approximately keep the FLOPS the same across all scenarios
+        // The dimensions are made extreme to highlight deficiencies in concurrent implementations
+        switch (shape) {
+            case "square" -> M = N = P = (int)(1.587*size);
+            case "tall" -> {
+                M = 4*size;
+                N = blockLength;
+                P = size*size/blockLength;
+            }
+            case "wide" -> {
+                M = blockLength;
+                N = size*size/blockLength;
+                P = 4*size;
+            }
+            case "long" -> {
+                M = size*size/blockLength;
+                N = 4*size;
+                P = blockLength;
+            }
+            default -> throw new RuntimeException("Unknown option");
+        }
+
+        A = MatrixOps_DDRB.createRandom(M, N, -1, 1, rand, blockLength);
+        B = MatrixOps_DDRB.createRandom(N, P, -1, 1, rand, blockLength);
+        C = MatrixOps_DDRB.createRandom(M, P, -1, 1, rand, blockLength);
 
         Asub = new DSubmatrixD1(A, 0, A.numRows, 0, A.numCols);
         Bsub = new DSubmatrixD1(B, 0, B.numRows, 0, B.numCols);
         Csub = new DSubmatrixD1(C, 0, C.numRows, 0, C.numCols);
     }
 
-    @Benchmark
-    public void mult() {
+    /// Adjust the shape so that
+    private void adjustShapes( boolean transA, boolean transB ) {
+        A.reshape(transA ? N : M, transA ? M : N);
+        B.reshape(transB ? P : N, transB ? N : P);
+        Asub = new DSubmatrixD1(A, 0, A.numRows, 0, A.numCols);
+        Bsub = new DSubmatrixD1(B, 0, B.numRows, 0, B.numCols);
+    }
+
+    @Benchmark public void mult() {
         MatrixMult_MT_DDRB.mult(blockLength, Asub, Bsub, Csub);
     }
 
-    @Benchmark
-    public void multMinus() {
-        MatrixMult_MT_DDRB.multMinus(blockLength, Asub, Bsub, Csub);
-    }
-
-    @Benchmark
-    public void multPlus() {
-        MatrixMult_MT_DDRB.multPlus(blockLength, Asub, Bsub, Csub);
-    }
-
-    @Benchmark
-    public void multMinusTransA() {
-        MatrixMult_MT_DDRB.multMinusTransA(blockLength, Asub, Bsub, Csub);
-    }
-
-    @Benchmark
-    public void multPlusTransA() {
-        MatrixMult_MT_DDRB.multPlusTransA(blockLength, Asub, Bsub, Csub);
-    }
-
-    @Benchmark
-    public void multTransA() {
+    @Benchmark public void multTransA() {
+        adjustShapes(true, false);
         MatrixMult_MT_DDRB.multTransA(blockLength, Asub, Bsub, Csub);
     }
 
-    @Benchmark
-    public void multTransB() {
+    @Benchmark public void multTransB() {
+        adjustShapes(false, true);
         MatrixMult_MT_DDRB.multTransB(blockLength, Asub, Bsub, Csub);
+    }
+
+    @Benchmark public void multMinus() {
+        MatrixMult_MT_DDRB.multMinus(blockLength, Asub, Bsub, Csub);
+    }
+
+    @Benchmark public void multMinusTransA() {
+        adjustShapes(true, false);
+        MatrixMult_MT_DDRB.multMinusTransA(blockLength, Asub, Bsub, Csub);
+    }
+
+    @Benchmark public void multMinusTransB() {
+        adjustShapes(false, true);
+        MatrixMult_MT_DDRB.multMinusTransB(blockLength, Asub, Bsub, Csub);
+    }
+
+    @Benchmark public void multPlus() {
+        MatrixMult_MT_DDRB.multPlus(blockLength, Asub, Bsub, Csub);
+    }
+
+    @Benchmark public void multPlusTransA() {
+        adjustShapes(true, false);
+        MatrixMult_MT_DDRB.multPlusTransA(blockLength, Asub, Bsub, Csub);
+    }
+
+    @Benchmark public void multPlusTransB() {
+        adjustShapes(false, true);
+        MatrixMult_MT_DDRB.multPlusTransB(blockLength, Asub, Bsub, Csub);
     }
 
     public static void main( String[] args ) throws RunnerException {
